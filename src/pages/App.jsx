348ec0d838a1bcda690bad5c5e6a788671f1a458
RafaeleRibeiro <rafaeleribeiro212@gmail.com>
2023-08-11 10:20:24 -0300

    Desenvolvido o profile

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,10 +1,22 @@
-import "./App.css";
+import { useState } from "react";
 import Profile from "../components/profile/profile";
 
-export default function App() {
-    return(
-        <>
-      <Profile>@rrrfaele</Profile>
-        </>
-    );
+import "./App.css";
+
+import imgDarkMode from "../img/avatar.jpg"
+import imgLigthMode from "../img/avatar2.jpg"
+
+const App = () => {
+  const [isLigthMode,setIsLigthMode] = useState(false);
+
+const imgSrc = isLigthMode ? imgLigthMode :imgDarkMode;
+const imgAlt = isLigthMode ? "Imagem no modo Claro" : "Imagem no modo escuro";
+  return(
+    <div>
+      <Profile imgSrc={imgSrc} imgAlt={imgAlt}>@rrrfaele</Profile>
+    </div>
+  );
 };
+
+export default App;
+
